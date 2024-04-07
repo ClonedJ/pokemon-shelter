5694d7d29e8f4bef0e27bf2917daa91c1c9afa33
ClonedJ <103239500+ClonedJ@users.noreply.github.com>
2024-04-07 20:59:36 +0800

    Feat: Integrate package data into Package page

--- src/views/Package.vue
+++ src/views/Package.vue
@@ -1,4 +1,16 @@
-<script setup></script>
+<script setup>
+import packages from "../assets/data/packageData.json";
+</script>
 <template>
-  <h1>PACKAGE</h1>
+  <div v-for="(packagee, i) in packages.data" :key="i">
+    <h1>{{ packagee.name }}</h1>
+    <h3>{{ packagee.description }}</h3>
+    <br />
+    <h2>Services:</h2>
+    <div v-for="(service, j) in packagee.services" :key="j">
+      <h3>{{ j + 1 + ". " + service }}</h3>
+    </div>
+    <br />
+    <br />
+  </div>
 </template>
