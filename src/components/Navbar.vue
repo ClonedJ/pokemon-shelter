1a46169bca892b15a50c001d3c8a37c5c1c90ee0
annfont0 <fontanillajasmine00@gmail.com>
2024-04-11 07:11:11 +0800

    Fix navbar and add ListItem component

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,9 +3,9 @@ import { RouterLink } from "vue-router";
 import { currentUser, logout } from "@/utils/user";
 
 const items = {
-  employee: ["Home", "Tasks", "Trainers", "Reservations", "Logout"],
-  trainer: ["Home", "Package", "Gallery", "About", "Logout"],
-  guest: ["Home", "Package", "Gallery", "About", "Login"],
+  employee: ["home", "tasks", "trainers", "reservations", "logout"],
+  trainer: ["home", "package", "gallery", "about", "logout"],
+  guest: ["home", "package", "gallery", "about", "login"],
 };
 
 const colors = ["lime-800", "yellow-900", "amber-400", "cyan-600", "cyan-600"];
@@ -16,8 +16,8 @@ const colors = ["lime-800", "yellow-900", "amber-400", "cyan-600", "cyan-600"];
     <RouterLink
       v-for="(item, index) in items[currentUser ? currentUser.type : 'guest']"
       :key="index"
-      @click="item == 'Logout' && logout()"
-      :to="`/${(item != 'Home' || item != 'Logout') && item}`"
+      @click="item == 'logout' && logout()"
+      :to="`/${item == 'home' || item == 'logout' ? '' : item}`"
       active-class="active"
       :class="`px-12 py-4 uppercase font-semibold rounded-full hover:bg-${colors[index]}`"
     >
