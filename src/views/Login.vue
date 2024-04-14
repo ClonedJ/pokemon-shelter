db63f768b3fde953f18c7f8d68338753c0542b97
annfont0 <fontanillajasmine00@gmail.com>
2024-04-14 15:53:18 +0800

    CSS login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { validateUserCred, login } from "@/utils/user.js";
 import { ref } from "vue";
-import { useRouter } from "vue-router";
+import { useRouter, RouterLink } from "vue-router";
 
 const router = useRouter();
 
@@ -25,17 +25,37 @@ const submit = (event) => {
 
 <template>
   <div class="bg-image bg-image-1"></div>
-  <div class="right-section w-1/2"></div>
-  <div class="w-1/2 flex">
-    <form class="bg-white rounded-2xl">
-      <label for="username">Username:</label>
-      <input type="text" v-model="form.username" /><br />
+  <div class="flex grow w-full">
+    <div class="w-1/2 p-16 flex">
+      <form
+        class="bg-white w-full h-full p-4 rounded-2xl flex flex-col bg-opacity-80"
+      >
+        <RouterLink class="self-start" to="/">
+          <img alt="Back" src="/src/assets/images/arrow.png" class="w-8 h-8" />
+        </RouterLink>
 
-      <label for="password">Password:</label>
-      <input type="password" v-model="form.password" /><br />
+        <div class="text-center">
+          <h1>Welcome</h1>
+          <p>Lorem ipsum</p>
+        </div>
 
-      <button @click="submit($event)">Login</button>
-    </form>
+        <div class="flex flex-col">
+          <label for="username">Username</label>
+          <input type="text" v-model="form.username" /><br />
+
+          <label for="password">Password</label>
+          <input type="password" v-model="form.password" /><br />
+
+          <button
+            @click="submit($event)"
+            class="bg-amber-400 font-bold rounded-full py-2 text-white"
+          >
+            LOG IN
+          </button>
+        </div>
+      </form>
+    </div>
+    <div class="right-section w-1/2"></div>
   </div>
 </template>
 
@@ -43,6 +63,10 @@ const submit = (event) => {
 .right-section {
   background-image: url("/src/assets/images/snorlax.jpg");
   background-repeat: no-repeat;
-  @apply h-[100svh] fixed right-0;
+  @apply h-[100svh];
+}
+
+input {
+  @apply p-2 rounded-lg;
 }
 </style>
