ae5680e4bef158d1852d106adb5761cc2620f416
ClonedJ <103239500+ClonedJ@users.noreply.github.com>
2024-04-22 03:37:59 +0800

    Feat: Add Content in Account-Related Vues

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { RouterLink, useRoute } from "vue-router";
+import { logout } from "@/utils/user";
+import { watch } from "vue";
+
+const items = [
+  { label: "My Profile", route: "account/my-profile" },
+  { label: "Reservations", route: "account/my-reservations" },
+  { label: "Logout", route: "" },
+];
+const colors = ["lime-800", "yellow-900", "lime-800"];
+const router = useRoute();
+watch(router);
+</script>
+
+<template>
+  <div class="bg-white w-1/10 p-4">
+    <RouterLink
+      v-for="(item, index) in items"
+      :key="index"
+      @click="item.label == 'Logout' && logout()"
+      :to="`/${item.route}`"
+      :active-class="`bg-${colors[index]} text-white`"
+      :class="`block hover:bg-${colors[index]} hover:text-white`"
+    >
+      {{ item.label }}
+    </RouterLink>
+  </div>
+</template>
--- src/views/Account/MyReservations.vue
+++ src/views/Account/MyReservations.vue
@@ -1,5 +1,27 @@
-<script setup></script>
+<script setup>
+import Sidebar from "@/components/Sidebar.vue";
+import { currentUser } from "@/utils/user";
+import { reservations } from "@/utils/reservation";
+import Table from "@/components/Table.vue";
+import { parseTable } from "@/utils/dataParser";
+
+const myReservations = reservations.filter(
+  (reservation) =>
+    reservation.user === currentUser.id ||
+    reservation.user.id === currentUser.id
+);
+
+const fields = [
+  { label: "id", data: "id" },
+  { label: "pokemon", data: "pokemon.name" },
+  { label: "package", data: "package" },
+  { label: "check in", data: "reservation.checkInDate" },
+  { label: "check out", data: "reservation.checkOutDate" },
+];
+</script>
 <template>
   <div class="bg-image bg-image-1"></div>
   <h1>My Reservations</h1>
+  <Sidebar />
+  <Table :fields="fields" :records="parseTable(myReservations)" />
 </template>
