40a3b3ed671131eb44b2760271906e8d8cfb9af9
ClonedJ <103239500+ClonedJ@users.noreply.github.com>
2024-04-26 20:30:27 +0800

    Feat: Add route guards

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,7 @@ import ReservationView from "@/views/Reservation.vue";
 import MyProfileView from "@/views/Account/MyProfile.vue";
 import MyReservationsView from "@/views/Account/MyReservations.vue";
 import LoginView from "@/views/Login.vue";
+import { currentUser } from "@/utils/user";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,58 +19,80 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+      meta: { requiredType: ["guest", "trainer", "employee"] },
     },
     {
       path: "/package",
       name: "package",
       component: PackageView,
+      meta: { requiredType: ["guest", "trainer"] },
     },
     {
       path: "/package/:id",
       name: "package-reserve",
       component: PackageReserveView,
+      meta: { requiredType: ["guest", "trainer"] },
     },
     {
       path: "/gallery",
       name: "gallery",
       component: GalleryView,
+      meta: { requiredType: ["guest", "trainer"] },
     },
     {
       path: "/about",
       name: "about",
       component: AboutView,
+      meta: { requiredType: ["guest", "trainer"] },
     },
     {
       path: "/tasks",
       name: "tasks",
       component: TaskView,
+      meta: { requiredType: ["employee"] },
     },
     {
       path: "/trainers",
       name: "trainers",
       component: TrainerView,
+      meta: { requiredType: ["employee"] },
     },
     {
       path: "/reservations",
       name: "reservations",
       component: ReservationView,
+      meta: { requiredType: ["employee"] },
     },
     {
       path: "/account/my-profile",
       name: "account/my-profile",
       component: MyProfileView,
+      meta: { requiredType: ["trainer"] },
     },
     {
       path: "/account/my-reservations",
       name: "account/my-reservations",
       component: MyReservationsView,
+      meta: { requiredType: ["trainer"] },
     },
     {
       path: "/login",
       name: "login",
       component: LoginView,
+      meta: { requiredType: ["guest"] },
     },
   ],
+});
+
+// Route guard
+router.beforeEach((to, from, next) => {
+  const userType = currentUser.type;
+
+  if (to.meta.requiredType.includes(userType)) {
+    next();
+  } else {
+    next({ name: "home" });
+  }
 });
 
 export default router;
